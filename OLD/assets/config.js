67e542e9ead8d7dda2a1306b2b9c76f46dc9c0a6
JuanPabloReyesgom <53346879+JuanPabloReyesgom@users.noreply.github.com>
2022-05-22 23:14:08 +0200

    web qalaub V1.0.0
    web qalaub V1.0.0

---
+++ OLD/assets/config.js
@@ -0,0 +1,40 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "+34647423629",
+        fax: "",
+        email: "info@qalaub.es",
+        address: "",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "QALaub",
+            title: "Quality Assurance",
+            subtitle: "Software Test",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
